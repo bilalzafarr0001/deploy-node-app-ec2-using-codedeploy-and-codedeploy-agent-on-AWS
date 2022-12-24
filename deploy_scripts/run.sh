50da8b4a55dc5c234c8ce1354b40f455c853a948
muhammad bilal <muhammadbilal@muhammads-MacBook-Pro.local>
2022-12-24 23:33:29 +0500

    Add appspec and deploy_scripts files..

--- deploy_scripts/run.sh
+++ deploy_scripts/run.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm install --lts
+nvm install 14
 cd /home/ec2-user/node
 npm install
 npm start
