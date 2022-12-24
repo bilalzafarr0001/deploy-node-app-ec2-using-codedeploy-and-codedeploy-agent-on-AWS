bf725a384182b150894e1e0627169ab9eef02c94
muhammad bilal <muhammadbilal@muhammads-MacBook-Pro.local>
2022-12-25 00:35:04 +0500

    update validate.sh file

--- deploy_scripts/install.sh
+++ deploy_scripts/install.sh
@@ -5,15 +5,10 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
 nvm install --lts
 nvm install 14
-
-# install pm2 module globaly
 npm install -g pm2
 pm2 update
-
-# delete existing bundle
 cd /home/ec2-user
 rm -rf node
 
